f841e50d8337334fa320a5e6ab0a52014eb39e10
Filip <becaphilippe@gmail.com>
2022-03-18 17:37:45 +0100

    corrected plot_arc function, added example

--- testing/plots/example_arc.m
+++ testing/plots/example_arc.m
@@ -1,5 +1,5 @@
-%EXAMPLE shows how to use GENERATE_RANDOM_Q, GENERATE_RANDOM_X,
-%PLOT_MATHCAL_G, PLOT_X_STAR, PLOT_LEVEL_SETS and PLOT_ARC.
+%EXAMPLE_ARC shows how to use GENERATE_RANDOM_Q, GENERATE_RANDOM_X,
+%PLOT_X_STAR, and PLOT_ARC.
 clear all;
 close all;
 clc;
@@ -7,12 +7,9 @@ clc;
 % Dimension
 n = 2;
 % Number of random matrices to generate
-nf = 5;
+nf = 2;
 
 addpath('../../plotting/');
-
-
-
 
 % Get random Q's and x's
 Q = generate_random_Q(n, nf, [0.5 3]);
@@ -24,22 +21,11 @@ fig.Position(1:2) = fig.Position(1:2) / 2;
 fig.Position(3:4) = [720, 540];
 hold all
 
-% Plot mesh
-n_mesh = 750;
-h_combined_sol = plot_mathcal_G(Q, x_star, n_mesh, 2);
-h_combined_sol.DisplayName = sprintf('$\\mathcal{G} =  \\{x | \\alpha_{%d : %d} \\} $', 1, nf);
-h_combined_sol.LineStyle = 'none';
-h_combined_sol.FaceAlpha = 0.5;
+%%%%%%%%%%%%% BEGIN Interesting Part
 
 % Plot solutions
-h_sol = plot_x_star(x_star, 150, winter(nf));
+h_sol = plot_x_star(x_star, 150, copper(nf));
 h_sol.DisplayName = sprintf('$x^*_{1:%d}$', length(x_star));
-
-% Plot level sets
-h_lev = plot_level_sets(Q, x_star, 1000, 1, winter(nf));
-for ii = 1 : length(h_lev)
-    h_lev(ii).HandleVisibility = 'Off';
-end
 
 % Plot arc
 randarc = [1 nf];
@@ -49,6 +35,9 @@ alpha_hess = linspace(0, 1, 5);
 for ii = 1 : length(h_hess)
     h_hess(ii).HandleVisibility = 'Off';
 end
+
+%%%%%%%%%%%%% END
+
 % Esthetics
 if n == 3
     view(3)
@@ -58,7 +47,7 @@ end
 xlabel('$X_1$-axis', 'FontSize', 15, 'FontWeight', 'bold', 'Interpreter', 'LaTeX');
 ylabel('$X_2$-axis', 'FontSize', 15, 'FontWeight', 'bold', 'Interpreter', 'LaTeX');
 zlabel('$X_3$-axis', 'FontSize', 15, 'FontWeight', 'bold', 'Interpreter', 'LaTeX');
-title(sprintf('Set of global optima $\\mathcal{G}$'), 'Interpreter', 'LaTeX');
+title(sprintf('Set of global optima $\\mathcal{G} - Arc between two functions$'), 'Interpreter', 'LaTeX');
 legend('Interpreter', 'LaTeX', 'Location', 'eastoutside');
 grid;
 axis square
